0fd987b7f9bdccca0fc87ce130d3145369e71f5b
rawad663 <rawad.karam663@gmail.com>
2019-02-04 18:44:55 -0500

    integrated react-ace

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import AceEditor from 'react-ace';
+import ace from 'brace';
+import 'brace/mode/java';
+import 'brace/theme/github';
+
 import './css/App.css';
 
 class App extends Component {
+
+    state = {
+        code: '// code'
+    };
+
+	onChange = (newValue) => {
+		this.setState({
+            code: newValue
+        });
+	};
 
     render() {
         return (
@@ -14,6 +29,16 @@ class App extends Component {
                     <p className="title">Hello,</p>
                     <p className="subtitle">Welcome to the Viatra frontend project</p>
                 </div>
+
+				<AceEditor
+                    style={{ width: '100%' }}
+					mode="javascript"
+                    value={this.state.code}
+					theme="github"
+					onChange={this.onChange}
+					name="UNIQUE_ID_OF_DIV"
+					editorProps={{$blockScrolling: true}}
+				/>,
             </div>
         )
     }
