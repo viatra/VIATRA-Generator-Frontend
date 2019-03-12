cd701da18ebd55c54596b19317812b28d1094bbc
rawad663 <rawad.karam663@gmail.com>
2019-03-12 14:45:08 +0100

    Added 2 new routes: - One for fetching all .ecore files - Another for fetching config information related to a specific input

--- backend/src/controllers/helpers.js
+++ backend/src/controllers/helpers.js
@@ -55,9 +55,16 @@ const searchAndReplaceFile = (file, match, replacement) => {
     }); 
 }
 
+/**
+ * Function to build url for fetching output generated:
+ * Either individual file (if provided)
+ * Or complete directory
+ * @param {string} dirPath 
+ * @param {string} logicalName 
+ */
 const buildOutputUrls = (dirPath, logicalName) => {
-    // function to build the route with or without file
-    const route = (file = null) =>(
+    // 
+    const route = (file = null) => (
         file 
         ? `http://localhost:8000/fetch/output/resource?logicalName=${logicalName}&file=${file}`
         : `http://localhost:8000/fetch/output/resource?logicalName=${logicalName}`
@@ -78,9 +85,62 @@ const buildOutputUrls = (dirPath, logicalName) => {
     });
 }
 
+/**
+ * Function that will parse a given .vsconfig file and 
+ * return the current (editable) values in it.
+ * @param {object} rd : The reader object used to read lines of file
+ */
+const parseVSConfig = (rd) => {
+    // Config values that will be returned to the user
+    const config = {
+        epackage: undefined,
+        vql: undefined,
+        scope: {
+            node: undefined
+        },
+        number: undefined,
+        runs: undefined
+    }
+    // Used to identify the correct line in the file
+    const targetIdentifier = {
+        epackage: '.ecore',
+        vql: '.vql',
+        node: '#node',
+        number: 'number',
+        runs: 'runs'
+    }
+
+    return new Promise((resolve, reject) => {
+        // Will match each line to the values of the config file that
+        // will be sent to the user, and update the config variable
+        // accordingly.
+        rd.on('line', (line) => {
+            if (line.includes(targetIdentifier.epackage)) 
+                config.epackage = line.slice(line.indexOf('"') + 1, line.length - 1);
+
+            else if (line.includes(targetIdentifier.vql))
+                config.vql = line.slice(line.indexOf('"') + 1, line.length - 1);
+
+            else if (line.includes(targetIdentifier.node))
+                config.scope.node = line.slice(line.indexOf('=') + 1, line.length).trim();
+
+            else if (line.includes(targetIdentifier.number))
+                config.number = line.slice(line.indexOf('=') + 1, line.length).trim();
+
+            else if (line.includes(targetIdentifier.runs))
+                config.runs = line.slice(line.indexOf('=') + 1, line.length).trim();
+        });
+
+        // We finished parsing the file, we can now send the result back
+        rd.on('close', () => resolve(config));
+    });
+}
+
+
 module.exports = {
     generateUID,
     saveInputFilesToDir,
     searchAndReplaceFile,
-    buildOutputUrls
+    buildOutputUrls,
+    parseVSConfig
 }
